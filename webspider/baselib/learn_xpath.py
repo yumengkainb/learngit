c21422b57d243c5261760773481377daf1d6ec3e
yumengkainb <513942583@qq.com>
2019-06-02 18:55:38 +0800

    20190602

--- webspider/baselib/learn_xpath.py
+++ webspider/baselib/learn_xpath.py
@@ -1,26 +1,27 @@
 import requests
-import lxml
+import json
+from lxml import etree
 
 def get_one_page(url):
     headers = {'User_Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
     request = requests.get(url=url,headers=headers)
-    print(request)
     if request.status_code == 200:
        return request.text
     return none
 
 def main():
     url =  'https://maoyan.com/board/4'
-    get_one_page(url)
-    html=etree.HTML(get_one_page(url),etree.HTMLParser())
-    result=html.xpath('//div')
+    content=get_one_page(url)
+    write_to_file(content)
+    html=etree.parse('./test.html',etree.HTMLParser())
+    result=html.xpath('//a[@href="/films/1203"]/@data-val')
     print(result)
-    print(result[0])
-    ##for item in get_one_page(url):
-        ##write_to_file(item) 
-        
+
 
 def write_to_file(content):
-    with open('result.txt','a',encoding='utf-8') as f:
-         f.write(json.dumps(content,ensure_ascii=False)+'\n')
+    with open ('./test.html','w',encoding='utf-8') as f:
+         f.write(content)
 
+
+if __name__=='__main__':
+   main()
